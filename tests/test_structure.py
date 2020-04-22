3ef7326ce075174a7d1ffe7b3f7542d4084d91d2
glims <bteodoru@gmail.com>
2020-04-22 17:51:55 +0300

    testing

--- tests/test_structure.py
+++ tests/test_structure.py
@@ -13,3 +13,13 @@ def test_add_beam():
     assert s.beams[0].length == 10, "beam length does not match input"
     assert s.beams[0].E == 29e6, "Young's modulus does not match input"
     assert s.beams[0].I == 350, "area moment of inertia does not match input"
+
+def test_add_nodal_load():
+
+    s = Structure('test')
+
+    s.add_nodal_load(value=10000, position=3, type='fz')
+
+    assert s.nodal_loads[0].value == 10000
+    assert s.nodal_loads[0].position == 3
+    assert s.nodal_loads[0].type == 'fz'
