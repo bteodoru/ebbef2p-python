6393b2db999ebf4beaf4453cef61e957251cef9a
glims <bteodoru@gmail.com>
2020-04-22 16:15:39 +0300

    docs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ebbef2p-python",
-    version="0.0.1.dev1",
+    version="0.0.2.dev1",
     author="Bogdan Teodoru",
     author_email="bteodoru@gmail.com",
     description="Tool for Finite Element Analysis of beams on elastic foundations",
